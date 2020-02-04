4ae1e7d73ee58d082b11f5092cde6ce1a00abc0b
Christopher Holmes <cdholmes@fsu.edu>
2020-02-04 17:33:18 -0500

    Add O1D+H2O = H2 + O2; Rewrite O1D branch ratios
    Reaction O1D+H2O has a small (0.006) yeild of H2+O2 (JPL 15-10)
    This reaction is 20-30% of chemical H2 production in the upper
    stratosphere, but only 2% near the surface.
    
    Rewrote rate coefficients for O1D+N2O and O1D+O3 so that the
    branching ratios appear in the KPP eqn rate expression, making
    the KPP file more easily comparable to JPL documentation.
    (For example, 1.2E-10 becomes 2.4E-10*0.5, where 0.5 is the branch
    ratio)
    No reaction rates were changed.
    
    Signed-off-by: Christopher Holmes <cdholmes@fsu.edu>

--- KPP/Standard/gckpp_Initialize.F90
+++ KPP/Standard/gckpp_Initialize.F90
@@ -13,7 +13,7 @@
 !        R. Sander, Max-Planck Institute for Chemistry, Mainz, Germany
 ! 
 ! File                 : gckpp_Initialize.f90
-! Time                 : Tue Feb  4 16:56:42 2020
+! Time                 : Tue Feb  4 17:31:38 2020
 ! Working directory    : /net/home_stu/cdholmes/gc-12.6.1/KPP/Standard
 ! Equation file        : gckpp.kpp
 ! Output root filename : gckpp
